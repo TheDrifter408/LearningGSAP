1328dc4fd3366507fbe0db391e486fedda7e25e4
Noor Ullah Al Noor <nurullahan408@gmail.com>
2024-12-01 13:24:28 +0600

    change: changed the widths of the container for different devices

--- src/app/components/utils/Container.tsx
+++ src/app/components/utils/Container.tsx
@@ -2,7 +2,7 @@ import { ContainerProps } from "@/types";
 
 export default function Container({ className, ...props }: ContainerProps){
     return(
-        <div className={`mx-auto px-4 sm:w-full md:w-[750px] lg:w-[1170px] ${className}`}>
+        <div className={`mx-auto px-4 sm:w-full md:w-[750px] lg:w-[85%] ${className}`}>
             {props.children}
         </div>
     )
